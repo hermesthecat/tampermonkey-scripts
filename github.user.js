26db8e93f6b4697a143ef1d53c64c33a63c31036
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2022-05-29 16:59:41 +0300

    Update github.user.js

--- github.user.js
+++ github.user.js
@@ -5,9 +5,8 @@
 // @description  github
 // @author       abdullahazad
 // @match        https://github.com/*
-// @resource     github https://github.com/fluidicon.png
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        GM_setClipboard
-// @grant        GM_notification
 // ==/UserScript==
 
 (function() {
@@ -16,6 +15,5 @@ if (document.querySelector("li.Box-row:nth-child(1) > tab-container:nth-child(3)
     var element = document.querySelector("li.Box-row:nth-child(1) > tab-container:nth-child(3) > div:nth-child(2) > div:nth-child(1) > input:nth-child(1)");
     var element2 = element.value;
     GM_setClipboard(element2);
-    //GM_notification("Github Copy Notification OK", element2, GM_getResourceURL("github"));
 }
 })();
