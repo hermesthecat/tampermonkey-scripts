71b8cf5cdf2567790a8b11f7868f940620c8aa09
abdullah azad <4947845+hermesthecat@users.noreply.github.com>
2023-05-02 10:38:12 +0300

    Update eksisozluk-v1.user.js

--- eksisozluk-v1.user.js
+++ eksisozluk-v1.user.js
@@ -1,12 +1,24 @@
 // ==UserScript==
 // @name        ekşi sözlük normal görünüme dön
-// @namespace   Auto click
-// @description Auto click
-// @include     https://eksisozluk.com/*
-// @include     https://eksisozluk.com
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=eksisozluk.com
+// @namespace   ekşi sözlük normal görünüme dön
+// @description ekşi sözlük normal görünüme dön
+// @version     0.2
+// @match       https://eksisozluk.com/*
+// @match       https://eksisozluk.com
+// @match       https://eksisozluk2023.com/*
+// @match       https://eksisozluk2023.com
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=eksisozluk.com
+// @require     https://code.jquery.com/jquery-2.1.3.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js
 // ==/UserScript==
- 
+
 setTimeout(function() {
-    document.querySelector('#return-to-innocence').click()
+
+    let CookieName = 'notheme'
+
+    if (!$.cookie(CookieName) || $.cookie(CookieName) == 0) {
+        $.removeCookie(CookieName);
+        $.cookie(CookieName, 1);
+        location.reload();
+    }
 }, 1000)
